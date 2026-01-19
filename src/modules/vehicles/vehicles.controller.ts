19c2917daaff138faa195f7f59c7094143e25200
Gianluca Paschoalotti <gianluca170703@gmail.com>
2026-01-19 19:43:09 -0300

    feat: adição de rota e delete na controller de vecihles, melhorias na user service

--- src/modules/vehicles/vehicles.controller.ts
+++ src/modules/vehicles/vehicles.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Post, Get, Body, Query, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { CreateVehicleDTO } from './dto/create-vehicle.dto';
 import { CreateVehicleResponseDTO } from './dto/create-vehicle-response.dto';
 import { ListVehiclesDTO } from './dto/list-vehicles.dto';
 import { ListVehiclesResponseDTO } from './dto/list-vehicles-response.dto';
+import { UpdateVehicleDTO } from './dto/update-vehicle.dto';
+import { UpdateVehicleResponseDTO } from './dto/update-vehicle-response.dto';
 
 @Controller('vehicles')
 export class VehiclesController {
@@ -11,12 +24,30 @@ export class VehiclesController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createVehicleDTO: CreateVehicleDTO): Promise<CreateVehicleResponseDTO> {
+  async create(
+    @Body() createVehicleDTO: CreateVehicleDTO,
+  ): Promise<CreateVehicleResponseDTO> {
     return this.vehiclesService.create(createVehicleDTO);
   }
 
   @Get()
-  async findAll(@Query() listVehiclesDTO: ListVehiclesDTO): Promise<ListVehiclesResponseDTO> {
+  async findAll(
+    @Query() listVehiclesDTO: ListVehiclesDTO,
+  ): Promise<ListVehiclesResponseDTO> {
     return this.vehiclesService.findAll(listVehiclesDTO);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateVehicleDTO: UpdateVehicleDTO,
+  ): Promise<UpdateVehicleResponseDTO> {
+    return this.vehiclesService.update(id, updateVehicleDTO);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.vehiclesService.delete(id);
   }
 }
